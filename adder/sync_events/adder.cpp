afcf4da4a2d03c5aa4328e23807ae54d662e15bc
Tiago Katcipis <tiagokatcipis@gmail.com>
2011-09-19 20:18:38 -0300

    yes yes

--- adder/sync_events/adder.cpp
+++ adder/sync_events/adder.cpp
@@ -3,13 +3,26 @@
 void adder::adder_process() {
   int  tmp1;
   int  tmp2;    
-  int  tmp3;       
   while ( true )  {
     tmp1 = in1.read();
     tmp2 = in2.read();
-    tmp3 = tmp1 + tmp2;
-    out1.write( tmp3 );        
-    cout << "\tadder: (" << tmp1 << "+" << tmp2 << "=" << tmp3 << ")";
-    wait();
+    result = tmp1 + tmp2;
+    cout << "\tadder: (" << tmp1 << "+" << tmp2 << "=" << result << ")";
+    sync_event.notify();
+    cout << "\tadder_process: waiting inc_process";
+    wait(sync_event);
+    cout << "adder: final result: (" << result << ")";
+    out1.write( result );
+    wait(clk);
+  }
+}
+
+void adder::inc_process() {
+  while ( true )  {
+    cout << "\n>>>inc: waiting adder\n";
+    wait(sync_event);
+    cout << "\n>>>inc: (" << result << " + 1 = " << result + 1 << ")\n";
+    result++;
+    sync_event.notify();
   }
 }
--- adder/sync_events/adder.h
+++ adder/sync_events/adder.h
@@ -8,13 +8,18 @@ SC_MODULE( adder )  {
   sc_in<int> in2;
   sc_out<int> out1;
   sc_in_clk   clk; 
+  sc_event sync_event;
+  int result;
  
   // Functionality
   void adder_process();
-  
+  void inc_process(); 
+ 
+
   // Constructor
   SC_CTOR( adder )  {
-    SC_CTHREAD( adder_process, clk.pos() );
+    SC_CTHREAD( adder_process, clk.pos());
+    SC_THREAD (inc_process);
   } 
 };
 
